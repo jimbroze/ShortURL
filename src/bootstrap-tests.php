29136c9eb3e1d41163c4e8bf430f02f6efb1e543
jimbroze <james.n.dickinson@gmail.com>
2023-02-12 20:55:27 +0000

    Changed testing bootstrap file to use correct autoload location

--- src/bootstrap-tests.php
+++ src/bootstrap-tests.php
@@ -1,6 +1,10 @@
 <?php
 // Bootstrap file for testing only.
 
-require_once 'bootstrap.php';
+require_once 'vendor/autoload.php';
+
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
 
 $_ENV["MYSQL_DATABASE"] = $_ENV["MYSQL_DATABASE_TEST"];
